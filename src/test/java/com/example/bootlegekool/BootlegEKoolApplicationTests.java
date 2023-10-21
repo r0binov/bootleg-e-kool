046f26c01cbf0cb0178df9dd825e5f1ffefaf2ac
r0binov <robinpall91@gmail.com>
2023-10-21 14:01:46 +0300

    initial spring boot commit

---
+++ src/test/java/com/example/bootlegekool/BootlegEKoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootlegekool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootlegEKoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
