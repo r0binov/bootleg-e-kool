c8bbb436a8bb0c46bd71adfbe3091cc89242987f
r0binov <robinpall91@gmail.com>
2023-10-21 14:40:19 +0300

    Revert "added missing files"
    This reverts commit 75619de397b4fbdead6dadfb2d6bb2b5f5865081.

---
+++ src/main/java/com/example/bootlegekool/BootlegEKoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootlegekool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootlegEKoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootlegEKoolApplication.class, args);
+	}
+
+}
